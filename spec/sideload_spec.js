4d8868cf7765b0a41a7a8dabd6c55173f2b13ef1
Philip Allen <philip.allen@searchdiscovery.com>
2014-07-31 11:28:24 -0400

    changed sideload to leave the original object unmodified

--- spec/sideload_spec.js
+++ spec/sideload_spec.js
@@ -1,7 +1,7 @@
 var sideload = require('../lib/sideload');
 var _ = require('lodash');
 
-var data = {
+var inputData = {
   presidents: [{
     id: 0,
     name: "Theodore Roosevelt",
@@ -32,11 +32,11 @@ var data = {
     }]
   }]
 };
+var backup = _.cloneDeep(inputData);
 
 describe('sideload()', function () {
   describe('operating on a "has one" relationship', function () {
-    // var data = _.clone(testData);
-    sideload(data, 'presidents.spouse');
+    var data = sideload(inputData, 'presidents.spouse');
 
     it('should create a top level plural-named array', function () {
       expect(_.isArray(data.spouses)).toBe(true);
@@ -47,13 +47,16 @@ describe('sideload()', function () {
     });
 
     it('should propogate the correct data to a top level key', function () {
-      expect(data.spouses[0].id).toBe(data.presidents[0].spouse);
+      expect(data.spouses[0]).toEqual(inputData.presidents[0].spouse);
+    });
+
+    it('should not modify the input object', function () {
+      expect(inputData).toEqual(backup);
     });
   });
 
   describe('operating on a "has many" relationship', function () {
-    // var data = _.clone(testData);
-    sideload(data, 'presidents.dogs');
+    var data = sideload(inputData, 'presidents.dogs');
 
     it('should create a top level plural-named array', function () {
       expect(_.isArray(data.dogs)).toBe(true);
@@ -64,7 +67,11 @@ describe('sideload()', function () {
     });
 
     it('should propogate the correct data to a top level key', function () {
-      expect(data.dogs[1].id).toBe(data.presidents[0].dogs[1]);
+      expect(data.dogs[1]).toEqual(inputData.presidents[0].dogs[1]);
+    });
+
+    it('should not modify the input object', function () {
+      expect(inputData).toEqual(backup);
     });
   });
 });
